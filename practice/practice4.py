b5c0f60fd6caccf9a2735602be864b4d537d8fb5
cloudgc <cloudgc@126.com>
2018-06-20 23:53:08 +0800

    add practice 20180612

--- practice/practice4.py
+++ practice/practice4.py
@@ -29,7 +29,7 @@ cyDf.head()
 '''
 
 newColumsDic = {'购药时间': 'time', '社保卡号': 'certNo', '商品编码': 'saleId',
-                '商品名称': 'saleName', '销售数': 'saleNum', '应收金额': 'chargeM',
+                '商品名称': 'saleName', '销售数量': 'saleNum', '应收金额': 'chargeM',
                 '实收金额': 'taken'}
 
 cyDf.rename(columns=newColumsDic, inplace=True)
@@ -40,5 +40,36 @@ cyDf.rename(columns=newColumsDic, inplace=True)
 '''
 
 # cyDf.shape
-cyDf.dropna(subset=['time', 'certNo'], how='any')
+cyDf = cyDf.dropna(subset=['time', 'certNo'], how='any')
 # cyDf.shape
+
+'''
+第四步：数据类型转换
+    删除缺失或者模型不足
+'''
+
+cyDf['saleNum'] = cyDf['saleNum'].astype('float')
+
+cytypes = cyDf.dtypes
+
+timeData = cyDf.loc[:, 'time']
+
+
+def splitTime(originalData):
+    timelist = []
+    for value in originalData:
+        dateStr = value.split(' ')[0]
+        timelist.append(dateStr)
+
+    timeClsSer = pd.Series(timelist)
+    return timeClsSer
+
+
+timeSeri = splitTime(timeData)
+
+cyDf.loc[:, 'time'] = timeSeri
+
+cyDf.loc[:, 'time'] = pd.to_datetime(cyDf.loc[:, 'time'], format='%Y-%m-%d', errors='coerce')
+
+cyDf=cyDf.dropna(subset=['time', 'certNo'], how='any')
+
